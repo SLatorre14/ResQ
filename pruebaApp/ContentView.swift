a4a617ea313980b5c78e26720c42baf722797c1d
SLatorre14 <slatorre@uniandes.edu.co>
2024-03-03 11:47:00 -0500

    Initial Commit

---
+++ pruebaApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pruebaApp
+//
+//  Created by IMAC on 3/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pruebaApp/pruebaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaAppApp.swift
+//  pruebaApp
+//
+//  Created by IMAC on 3/03/24.
+//
+
+import SwiftUI
+
+@main
+struct pruebaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
