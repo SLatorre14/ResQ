903f7fc0cf263d3a84e1c48c52dcd2bae493d629
SLatorre14 <slatorre@uniandes.edu.co>
2024-03-05 18:38:31 -0500

    proyecto

--- pruebaApp/ContentView.swift
+++ pruebaApp/ContentView.swift
@@ -8,19 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isClicked : Bool = false
+    let image = Image("resQLogo")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+     
+        ZStack {
+            
+            
+            Color("LightGreen")
+            RoundedRectangle(cornerRadius: 30, style: .circular)
+                .foregroundStyle(.linearGradient(colors: [Color("LighterGreen"), Color("LightGreen")], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20){
+                
+                Image("El")
+                    .offset(y:-80)
+                Text("resQ")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                Text("Security at your fingertips")
+                    .foregroundColor(.white)
+                
+              
+            }
+            Button{
+            }
+                label:{
+                    Text("Get Started")
+                        .foregroundColor(Color("LighterGreen"))
+                        .bold()
+                        .frame(width: 170, height: 50)
+                        .background{
+                            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                .fill(.white)
+                        }
+                }
+                .padding(.top)
+                .offset(y:120)
+                .buttonStyle(ScaleButtonStyle())
         }
-        .padding()
+       
+        
+        
+        
     }
+    
+        
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ScaleButtonStyle : ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View
+    {
+        configuration.label.scaleEffect(configuration.isPressed ? 2 : 1)
     }
 }
